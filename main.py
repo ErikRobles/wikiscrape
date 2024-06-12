24ffdef6dacea08f9573fb114a6c9b51e49f2c8b
ErikRobles <erikjames69@hotmail.com>
2024-06-12 15:56:21 -0600

    updated main.py file to gather all data

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from extract_us_states import USStatesExtractor
 from extract_mexico_info import MexicoExtractor
+from login_wikipedia import WikipediaLogin
 
 # Add debugging statements
 print("Starting script...")
@@ -19,12 +21,32 @@ print("Setting up Chrome driver...")
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Create an instance of MexicoExtractor and run the methods
-print("Extracting Mexico states data...")
-mexico_extractor = MexicoExtractor(driver)
-mexico_extractor.navigate_to_mexico_page()
-mexico_extractor.extract_mexico_data()
-print("Mexico states data extraction complete.")
+# Login to Wikipedia
+print("Logging into Wikipedia...")
+wikipedia_login = WikipediaLogin(driver)
+if wikipedia_login.login():
+    print("Login successful!")
+
+    # Create an instance of USStatesExtractor and run the methods
+    print("Extracting US states data...")
+    us_extractor = USStatesExtractor(driver)
+    us_extractor.navigate_to_us_states_page()
+    df_us_states = us_extractor.extract_us_states_data()
+    df_etimologia = us_extractor.extract_etimologia(df_us_states)
+    us_extractor.save_to_excel(df_us_states, df_etimologia)
+    print("US states data extraction complete.")
+
+    # Create an instance of MexicoExtractor and run the methods
+    print("Extracting Mexico states data...")
+    mexico_extractor = MexicoExtractor(driver)
+    mexico_extractor.navigate_to_mexico_page()
+    mexico_extractor.extract_mexico_data()
+    print("Mexico states data extraction complete.")
+
+    # Logout from Wikipedia
+    print("Logging out of Wikipedia...")
+    wikipedia_login.logout()
+    print("Logout successful!")
 
 # Close the browser
 print("Closing the browser...")
